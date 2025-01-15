58013a14b5bb090040b3ef9aab13e5d61ca9fc77
Chuck Smith <chuck@pinchaque.com>
2025-01-14 20:46:50 -0600

    Implement canvas with raw html & js as a starting point.

---
+++ src/canvas.js
@@ -0,0 +1,10 @@
+function drawCanvas(id) {
+    const canvas = document.getElementById(id);
+    const ctx = canvas.getContext("2d");
+
+    ctx.beginPath();
+    ctx.moveTo(20, 20);
+    ctx.lineTo(20, 100);
+    ctx.lineTo(70, 100);
+    ctx.stroke();
+}
