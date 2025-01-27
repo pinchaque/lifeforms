fe3c27081e956c6ced61ad03d989746c4650260a
Chuck Smith <chuck@pinchaque.com>
2025-01-26 20:52:34 -0700

    Placeholder Konva addition

--- app/controllers/env_controller.rb
+++ app/controllers/env_controller.rb
@@ -9,7 +9,9 @@ class EnvController < AppController
     env = Environment.where(id: id).first
 
     erb :"env/id", :locals => {
-      env: env, lifeforms: env.lifeforms.order(:name).all
+      env: env, 
+      lifeforms: env.lifeforms.order(:name).all,
+      scale: 3.0
     }
   end
 end
