2d75d788e0bb4e86dac118c4b5c0918fccdb7e72
Chuck Smith <chuck@pinchaque.com>
2025-02-20 21:22:34 -0700

    bugfix

--- app/sim/plant_factory.rb
+++ app/sim/plant_factory.rb
@@ -1,6 +1,5 @@
 class PlantFactory
   attr_accessor :energy_min, :energy_max
-  attr_accessor :growth_rate, :energy_split
   attr_accessor :size
   attr_accessor :env
   SPECIES_NAME = "Plant"
@@ -9,8 +8,6 @@ class PlantFactory
     @env = env
     @energy_min = 10.0
     @energy_max = 20.0
-    @energy_split = 30.0
-    @growth_rate = 0.2
     @size = 1.0
     @species = get_species
   end
@@ -31,10 +28,9 @@ class PlantFactory
     p.species_id = @species.id
     p.set_random_name
     p.set_loc_random
-    p.energy_absorb_perc = 0.5
-    p.energy_metabolic_rate = 5.0
-    p.energy_size_ratio = 2.0
-    p.growth_invest_perc = 0.2
+    p.energy_absorb_perc = 0.9
+    p.energy_base = 5.0
+    p.energy_reserve_perc = 0.7
     p.repro_threshold = 50.0
     p.repro_num_offspring = 3
     p.repro_energy_inherit_perc = 0.95
