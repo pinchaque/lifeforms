38c3dd690c851112699dac4ac76795e6315cf407
Chuck Smith <chuck@pinchaque.com>
2025-03-14 11:49:15 -0600

    WIP unit tests for env_energy

--- app/sim/skill/env_energy.rb
+++ app/sim/skill/env_energy.rb
@@ -3,7 +3,7 @@ module Skill
 
     def self.param_defs
       [
-        ParamDefNormalPerc(
+        Skill.ParamDefNormalPerc(
           id: :energy_absorb_perc,
           desc: "Percentage of the environmental energy available to this lifeform that it actually absorbs",
           mean: 0.95,
@@ -14,7 +14,9 @@ module Skill
 
     # The amount of energy lost due to overlaps with other lifeforms. This is
     # returned as a non-negative number.
-    def self.energy_overlap_loss(env, lf)
+    def self.overlap_loss(ctx)
+      lf = ctx.lifeform
+      env = ctx.env
       # To calculate the loss we add up the areas of all the overlapping 
       # lifeforms of this species. Then we divide that by two because 
       # the lifeforms are splitting the energy. Then we multiply by the
@@ -28,23 +30,27 @@ module Skill
         circle_area_intersect(lf.x, lf.y, lf.radius, oth.x, oth.y, oth.radius) }.sum
     end
 
+    # The gross amount of energy supplied to this lifeform based on its area
+    # alone, not taking into account overlaps with other lifeforms.
+    def self.env_gross(ctx)
+      ctx.env.energy_rate * ctx.lifeform.area
+    end
+
+    # Net energy available to us after subtracting off overlap loss; >= 0
+    def self.energy_net(ctx)
+      [0.0, env_gross(ctx) - overlap_loss(ctx)].max
+    end
+
+    # Calc out how much energy we can absorb from the environment
+    def self.energy_absorb(ctx)
+      energy_net(ctx) * ctx.value(:energy_absorb_perc)
+    end
+
+    # Adds our absorbable energy to the lifeform's energy stores. This is
+    # the total provided by the environment, minus that excluded by overlaps,
+    # times the absorbtion percentage.
     def self.exec(ctx)
-      env = ctx.env
-      lf = ctx.lifeform
-
-      # The gross amount of energy supplied to this lifeform based on its area
-      # alone, not taking into account overlaps with other lifeforms.
-      env_gross = env.energy_rate * lf.area
-
-      # energy lost to overlaps with other lifeforms
-      overlap_loss = energy_overlap_loss(env, lf)
-      
-      energy_net = [0.0, env_gross - overlap_loss].max
-
-      # Calc out how much energy we can absorb from the environment
-      energy_absorb = energy_net * ctx.value(:energy_absorb_perc)
-
-      lf.energy += energy_absorb
+      ctx.lifeform.energy += energy_absorb(ctx)
     end
   end
 end
--- spec/sim/skill/env_energy_spec.rb
+++ spec/sim/skill/env_energy_spec.rb
@@ -1,9 +1,147 @@
 include Skill
 
 describe "EnvEnergy" do
-  context "TODO" do
-    it "fails" do
-      expect(nil).to eq("need to implement unit tests")
+  let(:tol) { 0.0001 }
+  let(:species) { TestFactory.species }
+  let(:env_energy_rate) { 10.0 }
+  let(:env) { TestFactory.env(100, 100, 3, env_energy_rate) }
+  let(:klass) { EnvEnergy }
+  let(:skill_id) { klass.id }
+  let(:energy_absorb_perc) { 0.50 }
+
+  context "Generic Lifeform" do
+    let(:lf) { 
+      l = TestFactory.lifeform(env, species) 
+      l.register_skill(EnvEnergy)
+      l.params.fetch(:energy_absorb_perc).value = energy_absorb_perc
+      l
+    }
+    let(:ctx) { lf.context }
+
+    context "Lifeform.register_skill" do
+      it "registers successfully" do
+        expect(lf.skills.include?(skill_id))
+        klass.param_defs.each do |pd|
+          expect(lf.params.include?(pd.id)).to be true
+        end
+      end
+    end
+
+    context ".exec" do
+      it "has correct energy calcs" do
+        expect(lf.energy).to be_within(tol).of(10.0)
+
+        expect(lf.size).to be_within(tol).of(1.0)
+        expect(lf.radius).to be_within(tol).of(0.5)
+
+        # area = PI * (size/2)**2
+        expect(lf.area).to be_within(tol).of(0.7853981633974483)
+
+        # env_energy_rate * area
+        expect(klass.env_gross(ctx)).to be_within(tol).of(7.853981633974483)
+
+        # one lifeform => no overlaps
+        expect(klass.overlap_loss(ctx)).to be_within(tol).of(0.00)
+
+        # net = gross - overlaps = gross
+        expect(klass.energy_net(ctx)).to be_within(tol).of(7.853981633974483)
+
+        # energy_absorb = energy_net * absorb_perc
+        absorb_perc = lf.params.fetch(:energy_absorb_perc).value
+        expect(absorb_perc).to be_within(tol).of(0.5)
+        expect(klass.energy_absorb(ctx)).to be_within(tol).of(3.9269908169872414)
+
+        # run the action
+        klass.exec(ctx)
+
+        # energy = old_energy + energy_absorb
+        expect(lf.energy).to be_within(tol).of(13.9269908169872414)
+      end
+    end
+  end
+
+  context "energy calcs" do
+
+    def add_lf(x, y, size, energy)
+      l = TestFactory.lifeform(env, species) 
+      l.x = x
+      l.y = y
+      l.size = size
+      l.energy = energy
+      l.register_skill(EnvEnergy)
+      l.params.fetch(:energy_absorb_perc).value = energy_absorb_perc
+      l
+    end
+
+    it "single lifeform" do
+      lf = add_lf(10.0, 10.0, 1.0, 20.0)
+      ctx = lf.context
+      exp_egy = 7.853981633974483
+      exp_loss = 0.0
+      expect(klass.env_gross(ctx)).to be_within(tol).of(exp_egy)
+      expect(klass.overlap_loss(ctx)).to be_within(tol).of(exp_loss)
+      expect(klass.energy_net(ctx)).to be_within(tol).of(exp_egy - exp_loss)
+    end
+
+    it "two lifeforms, no overlap" do
+      lf0 = add_lf(10.0, 10.0, 1.0, 20.0)
+      lf1 = add_lf(20.0, 10.0, 1.0, 20.0)
+      ctx0 = lf0.context
+      ctx1 = lf1.context
+
+      exp_egy = 7.853981633974483
+      exp_loss = 0.0
+
+      expect(klass.env_gross(ctx0)).to be_within(tol).of(exp_egy)
+      expect(klass.overlap_loss(ctx0)).to be_within(tol).of(exp_loss)
+      expect(klass.energy_net(ctx0)).to be_within(tol).of(exp_egy - exp_loss)      
+
+      expect(klass.env_gross(ctx1)).to be_within(tol).of(exp_egy)
+      expect(klass.overlap_loss(ctx1)).to be_within(tol).of(exp_loss)
+      expect(klass.energy_net(ctx1)).to be_within(tol).of(exp_egy - exp_loss)      
+    end
+
+    it "two lifeforms, overlap" do
+      lf0 = add_lf(0.0, 0.0, 2.0, 20.0)
+      lf1 = add_lf(1.0, 0.0, 2.0, 20.0)
+      ctx0 = lf0.context
+      ctx1 = lf1.context
+      
+      overlap_area = circle_area_intersect(0, 0, 1, 1, 0, 1)
+
+      exp_egy = Math::PI * env_energy_rate
+      exp_loss = env_energy_rate * overlap_area / 2.0
+
+      expect(klass.env_gross(ctx0)).to be_within(tol).of(exp_egy)
+      expect(klass.overlap_loss(ctx0)).to be_within(tol).of(exp_loss)
+      expect(klass.energy_net(ctx0)).to be_within(tol).of(exp_egy - exp_loss)    
+      expect(klass.env_gross(ctx0)).to be >= 0.0  
+      expect(klass.overlap_loss(ctx0)).to be >= 0.0  
+      expect(klass.energy_net(ctx0)).to be >= 0.0  
+
+      expect(klass.env_gross(ctx1)).to be_within(tol).of(exp_egy)
+      expect(klass.overlap_loss(ctx1)).to be_within(tol).of(exp_loss)
+      expect(klass.energy_net(ctx1)).to be_within(tol).of(exp_egy - exp_loss)      
+      expect(klass.env_gross(ctx1)).to be >= 0.0  
+      expect(klass.overlap_loss(ctx1)).to be >= 0.0  
+      expect(klass.energy_net(ctx1)).to be >= 0.0  
+    end
+
+    it "many lifeforms with overlap" do
+      lf0 = add_lf(0.0, 0.0, 2.0, 20.0)
+      lf1 = add_lf(1.0, 0.0, 2.0, 20.0)
+      lf2 = add_lf(0.5, 0.5, 2.0, 20.0)
+      lf3 = add_lf(0.6, 0.6, 2.0, 20.0)
+      lf4 = add_lf(0.4, 0.4, 2.0, 20.0)
+    
+      exp_egy = Math::PI * env_energy_rate
+
+      [lf0, lf1, lf2, lf3, lf4].each do |lf|
+        ctx = lf.context
+        expect(klass.env_gross(ctx)).to be_within(tol).of(exp_egy)
+        expect(klass.overlap_loss(ctx)).to be >= 0.0  
+        expect(klass.energy_net(ctx)).to be_within(tol).of(0.0)
+      end
     end
   end
 end
