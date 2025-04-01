27b0b69ef03d443c3f0cc887fbc060ee94299ec1
Chuck Smith <chuck@pinchaque.com>
2025-04-01 14:23:02 -0500

    Update visualization to use images instead of circles (#22)
    Change the browser rendering to use cute tree and cow icons for Plant and Grazer, respectively. These will resize as the lifeforms grow.

--- public/js/render.js
+++ public/js/render.js
@@ -22,32 +22,50 @@ class Render {
     }
   }
 
-  lfColor(lf) {
-    switch(lf.species) {
-      case 'Plant':
-        return 'green'
-        break;
-      case 'Grazer':
-        return 'blue'
-        break;
-      default:
-        return 'black'
-        break;
-    }
-  }
-
-  addLifeform(lf) {
-    var circle = new Konva.Circle({
+  addDefaultShape(lf) {
+    let shape = new Konva.Circle({
       x: lf.x * this.zf,
       y: lf.y * this.zf,
       radius: lf.size / 2.0 * this.zf,
-      fill: this.lfColor(lf),
+      fill: 'blue',
       stroke: 'black',
       opacity: 0.5,
       strokeWidth: 1
     });
+    this.layer.add(shape)
+  }
 
-    this.layer.add(circle);
+  addImage(lf, imgURL) {
+    let layer = this.layer
+    let zf = this.zf
+    const imageObj = new Image();
+    imageObj.onload = function () {
+      const kImg = new Konva.Image({
+        x: (lf.x - (lf.size / 2.0)) * zf,
+        y: (lf.y - (lf.size / 2.0)) * zf,
+        image: imageObj,
+        width: lf.size * zf,
+        height: lf.size * zf,
+        opacity: 0.5
+      });
+    
+      layer.add(kImg);
+    };
+    imageObj.src = imgURL;
+  }
+
+  addLifeform(lf) {
+    switch(lf.species) {
+      case 'Plant':
+        return this.addImage(lf, "/images/tree.png")
+        break;
+      case 'Grazer':
+        return this.addImage(lf, "/images/cow.png")
+        break;
+      default:
+        return this.addDefaultShape(lf)
+        break;
+    }
   }
 
   render() {
